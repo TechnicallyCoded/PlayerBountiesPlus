563953088c5c5794f0a0abd0034d7c6dd3350bf0
TechnicallyCoded <technicallycoded@gmail.com>
2025-06-05 13:20:41 +0200

    Fix permissions indentation

---
+++ src/main/java/com/tcoded/playerbountiesplus/command/admin/bounty/AdminBountyDeleteCmd.java
@@ -0,0 +1,46 @@
+package com.tcoded.playerbountiesplus.command.admin.bounty;
+
+import com.tcoded.playerbountiesplus.PlayerBountiesPlus;
+import com.tcoded.playerbountiesplus.manager.BountyDataManager;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+import java.util.UUID;
+
+public class AdminBountyDeleteCmd {
+
+    private static final String PERMISSION = "playerbountiesplus.command.playerbountiesplus.bounty.delete";
+
+    public static boolean handleCmd(PlayerBountiesPlus plugin, CommandSender sender, Command cmd, String label, String[] args) {
+        if (!sender.hasPermission(PERMISSION)) {
+            String noPerm = plugin.getLang().getColored("command.no-permission");
+            String noPermDetailed = plugin.getLang().getColored("command.no-permission-detailed")
+                    .replace("{no-permission-msg}", noPerm)
+                    .replace("{permission}", PERMISSION);
+            sender.sendMessage(noPermDetailed);
+            return true;
+        }
+
+        if (args.length < 3) {
+            sender.sendMessage(plugin.getLang().getColored("command.admin.bounty.delete.missing-args"));
+            return true;
+        }
+
+        String playerName = args[2];
+        OfflinePlayer target = plugin.getServer().getOfflinePlayer(playerName);
+        if (target == null || (!target.hasPlayedBefore() && !target.isOnline())) {
+            sender.sendMessage(plugin.getLang().getColored("command.admin.bounty.delete.player-not-found"));
+            return true;
+        }
+
+        UUID uuid = target.getUniqueId();
+        BountyDataManager m = plugin.getBountyDataManager();
+        m.removeBounty(uuid);
+        m.saveBountiesAsync();
+
+        sender.sendMessage(plugin.getLang().getColored("command.admin.bounty.delete.success")
+                .replace("{target}", target.getName()));
+        return true;
+    }
+}
---
+++ src/main/java/com/tcoded/playerbountiesplus/command/admin/bounty/AdminBountySetCmd.java
@@ -0,0 +1,55 @@
+package com.tcoded.playerbountiesplus.command.admin.bounty;
+
+import com.tcoded.playerbountiesplus.PlayerBountiesPlus;
+import com.tcoded.playerbountiesplus.manager.BountyDataManager;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+import java.util.UUID;
+
+public class AdminBountySetCmd {
+
+    private static final String PERMISSION = "playerbountiesplus.command.playerbountiesplus.bounty.set";
+
+    public static boolean handleCmd(PlayerBountiesPlus plugin, CommandSender sender, Command cmd, String label, String[] args) {
+        if (!sender.hasPermission(PERMISSION)) {
+            String noPerm = plugin.getLang().getColored("command.no-permission");
+            String noPermDetailed = plugin.getLang().getColored("command.no-permission-detailed")
+                    .replace("{no-permission-msg}", noPerm)
+                    .replace("{permission}", PERMISSION);
+            sender.sendMessage(noPermDetailed);
+            return true;
+        }
+
+        if (args.length < 4) {
+            sender.sendMessage(plugin.getLang().getColored("command.admin.bounty.set.missing-args"));
+            return true;
+        }
+
+        String playerName = args[2];
+        int amount;
+        try {
+            amount = Integer.parseInt(args[3]);
+        } catch (NumberFormatException ex) {
+            sender.sendMessage(plugin.getLang().getColored("command.admin.bounty.set.amount-nan"));
+            return true;
+        }
+
+        OfflinePlayer target = plugin.getServer().getOfflinePlayer(playerName);
+        if (target == null || (!target.hasPlayedBefore() && !target.isOnline())) {
+            sender.sendMessage(plugin.getLang().getColored("command.admin.bounty.set.player-not-found"));
+            return true;
+        }
+
+        UUID uuid = target.getUniqueId();
+        BountyDataManager m = plugin.getBountyDataManager();
+        m.setBounty(uuid, amount);
+        m.saveBountiesAsync();
+
+        sender.sendMessage(plugin.getLang().getColored("command.admin.bounty.set.success")
+                .replace("{target}", target.getName())
+                .replace("{bounty}", String.valueOf(amount)));
+        return true;
+    }
+}
